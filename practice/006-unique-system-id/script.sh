fd7e440b2cabe9a39556ef5770e9d9a43197258e
charle <kidccc@gmail.com>
2015-09-10 22:02:13 +0800

    modify 006

--- practice/006-unique-system-id/script.sh
+++ practice/006-unique-system-id/script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-str="213t345ef45sa"
+str=`cat /etc/passwd|md5sum`
+#str="da7415ee73649d098e9721f298d2ef85"
 count=${#str}
-for((i=0;i<$count;i++))
+unique_id=""
+for((i=0; i<10; i++))
 do
     # printf "%s,%s" $i $j
     # echo ${str:$i:1}
     myint=`printf ${str:$i:1}`
     tmp=`printf "%d" "'$myint"`
-    if ((tmp > 47 && tmp < 58))
+    if (( tmp > 47 && tmp < 58 ))
     then
-        echo $myint"test"
+        unique_id=$unique_id$myint
     fi
 done
+echo $unique_id
