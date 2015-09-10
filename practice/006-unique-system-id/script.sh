7072a55948d83fee792778cac180f0e7858e76a1
程超 <charles.chengc@alibaba-inc.com>
2015-09-10 10:24:06 +0800

    add 006

---
+++ practice/006-unique-system-id/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+str="213t345ef45sa"
+count=${#str}
+for((i=0;i<$count;i++))
+do
+    # printf "%s,%s" $i $j
+    # echo ${str:$i:1}
+    myint=`printf ${str:$i:1}`
+    tmp=`printf "%d" "'$myint"`
+    if ((tmp > 47 && tmp < 58))
+    then
+        echo $myint
+    fi
+done
